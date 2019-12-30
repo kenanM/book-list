9fd05fadd08819326f94aded18762b2eb72c50de
Kenan McGrath <kenan.mcgrath@gmail.com>
2019-12-30 13:10:58 +0000

    Add SearchForm component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,33 @@ import React, { Component } from 'react';
 import './App.css';
 
 
+class SearchForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" value={this.state.value} onChange={this.handleChange} />
+      </form>
+    );
+  }
+}
+
 function SearchResult(props) {
   return (
     <div className="card border-dark mb-3">
@@ -44,7 +71,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <h2>Book List</h2>
-          <input type="text" />
+          <SearchForm />
         </div>
         <div class="container" style={{marginTop: 15}}>
           {renderedResults}
