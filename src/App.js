b87a2f11976b2dbaecf419207cb523a6f92ebc8a
Kenan McGrath <kenan.mcgrath@gmail.com>
2019-12-30 13:06:56 +0000

    Initial layout and styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
+function SearchResult(props) {
+  return (
+    <div className="card border-dark mb-3">
+      <div className="card-body">
+        <div className="card-title"> <h3> {props.title} </h3></div>
+        <div className="card-subtitle"> {props.author} </div>
+        <p className="card-text">{props.description}</p>
+      </div>
+    </div>
+  )
+}
+
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      'query': 'Harry Potter',
+      'searchResults': [
+        {'title': 'Harry Potter', 'author': 'JK Rowling', 'description': 'A young wizard...'},
+        {'title': 'Harry Potter', 'author': 'JK Rowling', 'description': 'A young wizard...'},
+        {'title': 'Harry Potter', 'author': 'JK Rowling', 'description': 'A young wizard...'}
+      ],
+    }
+  }
+
   render() {
+    const searchResults = this.state.searchResults;
+    const renderedResults = searchResults.map((result, index) => {
+      return (
+        <SearchResult key={index} 
+                      title={result.title}
+                      author={result.author}
+                      description={result.description}
+        />
+      )
+    });
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Book List</h2>
+          <input type="text" />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div class="container" style={{marginTop: 15}}>
+          {renderedResults}
+        </div>
       </div>
     );
   }
